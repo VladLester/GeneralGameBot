b759e70b4d668e2cfbd5d73f2e5e6d14d13b7e4f
Алексей <batyulevolexiy@outlook.com>
2021-10-26 21:19:47 +0300

    t

--- GeneralGameBot/Program.cs
+++ GeneralGameBot/Program.cs
@@ -13,18 +13,18 @@ namespace GeneralGameBot
         {
             client = new TelegramBotClient(TelegramBotData.token);
             client.StartReceiving();
-            client.OnMessage += async(object sender, MessageEventArgs e) => 
+            client.OnMessage += async (object sender, MessageEventArgs e) =>
             {
                 var msg = e.Message;
 
-                if (msg.Text != "Информация про генерала" && msg.Text != "Правила генеральской битвы" )
+                if (msg.Text != "Информация про генерала" && msg.Text != "Правила генеральской битвы")
                 {
                     try
                     {
-                        
+
                         await client.SendTextMessageAsync(chatId: msg.Chat.Id, File.ReadAllText(@"C:\Users\xlegolazxx\Desktop\general.txt"));
                     }
-                    catch(Exception exc)
+                    catch (Exception exc)
                     {
 
                         Console.WriteLine(exc.Message);
@@ -32,7 +32,7 @@ namespace GeneralGameBot
                 }
 
             };
-            Console.ReadLine();
+            Console.ReadLine();          
         }
 
        
