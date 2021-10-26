7c84d608276d050f54aa4666cda084b8b4550e4a
Lytosh Vlad <vllitosh46@gmail.com>
2021-10-26 13:16:58 +0300

    telegram123

--- GeneralGameBot/Program.cs
+++ GeneralGameBot/Program.cs
@@ -4,9 +4,9 @@ namespace GeneralGameBot
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
---
+++ GeneralGameBot/TelegramBotData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralGameBot
+{
+    static class TelegramBotData
+    {
+        public static readonly string token = "2038043873:AAHiToDZmC8X7nuq0Pc83AKy000Fcktg49w";
+
+    }
+}
