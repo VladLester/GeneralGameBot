e27b921105758ef0fbc8fbb7538ab909a1499da8
Lytosh Vlad <vllitosh46@gmail.com>
2021-10-26 13:09:23 +0300

    Добавьте файлы проекта.

---
+++ GeneralGameBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeneralGameBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
