578ce742b6f078082d6f4bc255a1750fa26ad1e0
Lytosh Vlad <vllitosh46@gmail.com>
2021-10-30 13:51:00 +0300

    вфыв

--- GeneralGameBot/TelegramBotData.cs
+++ GeneralGameBot/TelegramBotData.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
+using System.IO;
 
 namespace GeneralGameBot
 {
     static class TelegramBotData
     {
-        public static readonly string token = "2038043873:AAHiToDZmC8X7nuq0Pc83AKy000Fcktg49w";
+        public static readonly string token = File.ReadAllText(@"C:\GeneralGameBot\BotInfo.txt");
         
 
     }
